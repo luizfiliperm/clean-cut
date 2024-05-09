cf66456a410d449eb5263c9e795cb578dfd47ceb
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-09 09:04:38 -0300

    Adding Project files

---
+++ src/main/java/br/com/cc/cleancut/CleanCutApplication.java
@@ -0,0 +1,13 @@
+package br.com.cc.cleancut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cc/cleancut/CleanCutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cc.cleancut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
