21095253af7558dcf7f7bd2f9a6b5d038b883a3d
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-16 20:29:13 -0300

    Criando modelo de imagem

---
+++ src/main/java/br/com/cc/cleancut/model/Image.java
@@ -0,0 +1,25 @@
+package br.com.cc.cleancut.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String image;
+
+    private Boolean isPrivate;
+
+}
