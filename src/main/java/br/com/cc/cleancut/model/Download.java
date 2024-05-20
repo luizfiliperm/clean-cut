3afa45d1d6c6fd76be0c8a937cbead1c1a103949
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-20 20:45:41 -0300

    Adicionando Anotações no model Download

--- src/main/java/br/com/cc/cleancut/model/Download.java
+++ src/main/java/br/com/cc/cleancut/model/Download.java
@@ -1,6 +1,20 @@
 package br.com.cc.cleancut.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Download {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
