ffd557cf9c3d890ced506e4abffd63409be726ae
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-26 12:51:39 -0300

    Adding loggedUserContext

--- src/main/java/br/com/cc/cleancut/controllers/AuthController.java
+++ src/main/java/br/com/cc/cleancut/controllers/AuthController.java
@@ -3,15 +3,19 @@ package br.com.cc.cleancut.controllers;
 import br.com.cc.cleancut.model.User;
 import br.com.cc.cleancut.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@SessionAttributes("loggedUser") // Armazena "loggedUser" na sessão
 public class AuthController {
 
-    UserService userService;
+    private final UserService userService;
 
     public AuthController(UserService userService) {
         this.userService = userService;
@@ -19,7 +23,7 @@ public class AuthController {
 
     @GetMapping("/auth")
     public ModelAndView login() {
-        return new ModelAndView("pages/login");
+        return new ModelAndView("pages/authentication");
     }
 
     @PostMapping("/register")
@@ -27,20 +31,44 @@ public class AuthController {
             @RequestParam("name") String name,
             @RequestParam("email") String email,
             @RequestParam("password") String password,
-            @RequestParam("confirmPassword") String confirmPassword) {
+            @RequestParam("confirmPassword") String confirmPassword,
+            Model model) {
 
         try {
             User user = new User();
             user.setName(name);
             user.setEmail(email);
             user.setPassword(password);
+
             userService.register(user, confirmPassword);
+            model.addAttribute("loggedUser", user);
             return new ModelAndView("redirect:/");
         } catch (RuntimeException e) {
-            ModelAndView mv = new ModelAndView("pages/login");
-            mv.addObject("error", e.getMessage());
-            return mv;
+            ModelAndView mav = new ModelAndView("pages/authentication");
+            mav.addObject("error", e.getMessage());
+            return mav;
         }
     }
 
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam("email") String email,
+                              @RequestParam("password") String password,
+                              Model model) {
+        try {
+            // Autentica o usuário
+            User user = userService.login(email, password);
+            model.addAttribute("loggedUser", user);
+            return new ModelAndView("redirect:/");
+        } catch (RuntimeException e) {
+            ModelAndView mav = new ModelAndView("pages/authentication");
+            mav.addObject("error", e.getMessage());
+            return mav;
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(SessionStatus status) {
+        status.setComplete();
+        return "redirect:/";
+    }
 }
